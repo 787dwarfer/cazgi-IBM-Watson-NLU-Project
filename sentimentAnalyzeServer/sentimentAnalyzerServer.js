61456f1729a323f47c87fec85537798a78770d0f
787dwarfer <=>
2021-05-15 20:39:59 +0000

    updated files

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+function getNLUInstance() {
+   let api_key = process.env.API_KEY;
+   let api_url = process.env.API_URL;
+
+   const NaturalLanguageUnderstangingV1 = require('ibm-watson/natural-language-understanding/v1');
+   const {IamAuthenticator} = require('ibm-watson/auth');
+
+   const naturalLanguageUnderstanding = new NaturalLanguageUnderstangingV1({
+       version: '2021-05-15',
+       authenticator: new IamAuthenticator({
+           apikey:api_key,
+       }),
+       serviceUrl: api_url,
+   });
+}
 
 app.use(express.static('client'))
 
